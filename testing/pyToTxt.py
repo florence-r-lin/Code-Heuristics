339ef128e9861ab140f8edf131e00b670399e26b
florence-r-lin <148039626+florence-r-lin@users.noreply.github.com>
2024-10-30 13:29:45 -0700

    update directory

--- testing/pyToTxt.py
+++ testing/pyToTxt.py
@@ -3,21 +3,12 @@ import shutil
 
 
 # cd to summer-2024 directory
-main_directory = '/Users/summer-2024/Desktop/assignment_214232_export'  
+main_directory = '/Users/summer-2024/Desktop/assignments prellm/'  
 all_text_dir = os.path.join(main_directory, 'allText')
-
-if not os.path.exists(all_text_dir):
-    os.makedirs(all_text_dir)
-    print(f"Created directory: {all_text_dir}")
-else:
-    print(f"Directory already exists: {all_text_dir}")
 
 for root, dirs, files in os.walk(main_directory):
     for file in files:
-        if file == 'final.py':
-            print(f"Found final.py in: {root}")
+        if file.endswith('.py'):
             file_path = os.path.join(root, file)     
             new_file_path = os.path.join(all_text_dir, f"{os.path.basename(root)}_final.txt")
-            print(f"Copying {file_path} to {new_file_path}")
             shutil.copy(file_path, new_file_path)
-            print(f'Copied {file_path} to {new_file_path}')
