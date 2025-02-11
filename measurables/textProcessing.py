7059abaa518e8c9aaa7756f1d2ea05510d8780b8
edonson2016 <edonson2016@gmail.com>
2025-02-10 16:02:39 -0800

    Added Textprocessing to measurables

---
+++ measurables/textProcessing.py
@@ -0,0 +1,34 @@
+import os
+import json
+import fileParsing
+
+def isNotAlphaNumeric(s):
+    if "\n" == s[-1]:
+        return not s[:-1].isalnum()
+    else:
+        return not s.isalnum()
+
+def notebookToPy(filePath):
+    #Take all text files, seperate markdown and python into seperate files
+    #for file in os.listdir(filePath):
+    if filePath[-1] == "b":
+        #print(filePath)
+        with open(filePath, 'r') as tFile:
+            notebook = json.load(tFile)
+            #newT = open(filePath[:-6] + "-code.py", 'w')
+            newT = open(fileParson.backOneDir(filePath) + "/final.py", "w")
+            markT = open(filePath[:-6] + "-mark.txt", 'w')                
+            for cell in notebook['cells']:
+                if cell['cell_type'] == 'code':
+                    filtLines = list(filter(isNotAlphaNumeric, cell['source']))
+                    if "d\n" in filtLines:
+                        print(list(filtLines))
+                    newT.writelines(filtLines) 
+                else:
+                    markT.writelines(cell['source'])
+            newT.close()
+            markT.close()
+
+
+notebookToPy("/home/edonson/METRICLab/Code-Heuristics/studentScripts/notebooks/final_project.ipynb") #Takes path to notebook
+                        
