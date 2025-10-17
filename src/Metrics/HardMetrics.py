ae438d714d5279f619bc9b85838ba9bda6f0e5d4
florence-r-lin <148039626+florence-r-lin@users.noreply.github.com>
2025-10-16 20:47:08 -0700

    Final changes for Yuan

--- src/Metrics/HardMetrics.py
+++ src/Metrics/HardMetrics.py
@@ -519,10 +519,3 @@ def allMetrics(scriptPath, tree=None):
     'semester': Semester,
     'year': Year
 }
-
-
-
-
-
-# print(allMetrics('/Users/summer-2024/Desktop/code metrics 25/All-Data/CS35-Data/assignments postllm/submissions_cs35_sp2025/submission_351/final|hw4pr1 .py'))
-# ['filepath', 800, 24.75, 4.125, 37.25, 7, 12.857142857142858, 3, 3.0, 11, 1, 1.3178491592407227, 'CS35', 'sp2025', 2025]
